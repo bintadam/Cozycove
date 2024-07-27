2d0f8a3b8ab6d06c81b161d29d79e8bf73616c3c
bintadam <zakiyaadan12@gmail.com>
2024-07-27 18:46:06 +0200

    installed express and mongoose

--- cozycove/tailwind.config.js
+++ cozycove/tailwind.config.js
@@ -1,9 +1,16 @@
+const flowbite = require("flowbite-react/tailwind");
+
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{html,js,jsx}"],
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx}",
+    flowbite.content(),
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    flowbite.plugin(),
+  ],
 }
 
