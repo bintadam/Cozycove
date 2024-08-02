d9567c4725c6e6d2476b108a4cb7be96c4586470
bintadam <zakiyaadan12@gmail.com>
2024-08-02 17:04:43 +0200

    created a test api route

--- cozy-api/index.js
+++ cozy-api/index.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import mongoose from "mongoose"
 import dotenv from "dotenv"
-
+import userRoutes from './routes/user.route.js'
 const app = express()
+
+app.use(express.json())
 
 dotenv.config()
 
@@ -10,6 +12,11 @@ app.listen(3000, ()=>{
     console.log('Server is running on port 3000')
 })
 
+app.use('/api/user', userRoutes)
+
 mongoose.connect(process.env.MONGO)
     .then(()=>console.log('MongoDB is connected'))
     .catch(error=> console.log(error))
+
+
+
