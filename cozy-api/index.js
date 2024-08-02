8a9e93fecb00b21b81f6830b755093f047322fa9
bintadam <zakiyaadan12@gmail.com>
2024-08-02 16:39:48 +0200

    created pages

--- cozy-api/index.js
+++ cozy-api/index.js
@@ -1,7 +1,15 @@
 import express from 'express'
+import mongoose from "mongoose"
+import dotenv from "dotenv"
 
 const app = express()
+
+dotenv.config()
 
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000')
 })
+
+mongoose.connect(process.env.MONGO)
+    .then(()=>console.log('MongoDB is connected'))
+    .catch(error=> console.log(error))
